d75ed963ef71f44b31ec85364846e8bb2ecf8e58
Antonio Sucre <ajs2104@gmail.com>
2020-12-10 11:01:36 -0300

    EF Core e SQL Server

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,11 @@ namespace Shop
         {
             services.AddControllers();
 
-            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+            //services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+
+            services.AddDbContext<DataContext>(
+                opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString"))
+            );
 
             services.AddScoped<DataContext, DataContext>();
         }
