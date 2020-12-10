112617722ceaaf71d9e180ee78993c2a05d12275
Antonio Sucre <ajs2104@gmail.com>
2020-12-10 10:23:03 -0300

    Iniciando controller products

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            var products = await context.Products.Include(x => x.Category).AsNoTracking().ToListAsync();
+            return Ok(products);
+        }
+    }
+}
