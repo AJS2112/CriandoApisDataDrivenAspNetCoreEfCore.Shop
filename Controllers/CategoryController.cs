13fad32f3c8261e2c9d1fe714ddd3ee567d4097c
Antonio Sucre <ajs2104@gmail.com>
2020-12-10 00:16:37 -0300

    PUT e DELETE

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -26,14 +26,17 @@ public class CategoryController : ControllerBase
     }
 
     [HttpPut]
-    [Route("")]
-    public string Put()
+    [Route("{id:int}")]
+    public Category Put(int id, [FromBody] Category model)
     {
-        return "PUT";
+        if (model.Id == id)
+            return model;
+
+        return null;
     }
 
     [HttpDelete]
-    [Route("")]
+    [Route("{id:int}")]
     public string Delete()
     {
         return "DELETE";
